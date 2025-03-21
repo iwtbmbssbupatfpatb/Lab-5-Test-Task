0e4e110d66cc9d29878193df33741f85c58a17fa
Ирина <iradrankovich@gmail.com>
2025-03-21 23:22:45 +0300

    .

---
+++ Services/Implementations/SessionService.cs
@@ -0,0 +1,58 @@
+﻿using Lab5TestTask.Data;
+using Lab5TestTask.Models;
+using Lab5TestTask.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Lab5TestTask.Enums;
+
+namespace Lab5TestTask.Services.Implementations;
+
+/// <summary>
+/// SessionService implementation.
+/// Implement methods here.
+/// </summary>
+public class SessionService : ISessionService
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public SessionService(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<Session> GetSessionAsync()
+    {
+            return await _dbContext.Sessions
+                .Where(s => s.DeviceType == DeviceType.Desktop)
+                .OrderBy(s => s.StartedAtUTC)
+                .FirstOrDefaultAsync();
+                
+    }
+
+    
+
+    public async Task<List<Session>> GetSessionsAsync()
+    {
+        DateTime thresholdDate = new DateTime(2025, 1, 1);
+
+    var activeSessions = await _dbContext.Sessions
+        .AsNoTracking()
+        .Where(s => s.EndedAtUTC < thresholdDate && s.User.Status == UserStatus.Active)
+        .Select(s => new Session
+        {
+            Id = s.Id,
+            StartedAtUTC = s.StartedAtUTC,
+            EndedAtUTC = s.EndedAtUTC,
+            DeviceType = s.DeviceType,
+            UserId = s.UserId,
+            User = new User
+            {
+                Id = s.User.Id,
+                Email = s.User.Email,
+                Status = s.User.Status
+            }
+        })
+        .ToListAsync();
+
+    return activeSessions;
+    }
+}
---
+++ Services/Implementations/UserService.cs
@@ -0,0 +1,34 @@
+﻿using Lab5TestTask.Data;
+using Lab5TestTask.Models;
+using Lab5TestTask.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Lab5TestTask.Enums; 
+
+namespace Lab5TestTask.Services.Implementations;
+
+/// <summary>
+/// UserService implementation.
+/// Implement methods here.
+/// </summary>
+public class UserService : IUserService
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public UserService(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    public async Task<User> GetUserAsync()
+    {
+        return await _dbContext.Users
+        .OrderByDescending(u => u.Sessions.Count)
+        .FirstOrDefaultAsync();
+    }
+
+    public async Task<List<User>> GetUsersAsync()
+    {
+        return await _dbContext.Users
+        .Where(u => u.Sessions.Any(s => s.DeviceType == DeviceType.Mobile)) 
+        .ToListAsync();
+    }
+}
